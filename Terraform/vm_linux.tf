e1d23365fbd2e5824eee915f8e72f1ec14937a04
lgparedes <121614909+lgparedes@users.noreply.github.com>
2023-03-07 14:33:58 +0100

    Update vm_linux.tf

--- Terraform/vm_linux.tf
+++ Terraform/vm_linux.tf
@@ -30,8 +30,8 @@ resource "azurerm_network_interface" "nic" {
 }
 resource "azurerm_public_ip" "pip" {
   name                = "VIP"
-  location            = azurerm_resource_group.joselu.location
-  resource_group_name = azurerm_resource_group.joselu.name
+  location            = azurerm_resource_group.AzureLG.location
+  resource_group_name = azurerm_resource_group.AzureLG.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
